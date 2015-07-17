7d90e3610e77482d20520112bbd56bbf6f8861cf
Brian Rogstad <brian.rogstad@thomsonreuters.com>
2015-07-17 14:19:18 -0500

    added some CSS animations and transitions

--- client/assets/js/init.js
+++ client/assets/js/init.js
@@ -1,17 +1,29 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
-	$('.open-menu').click(function(){
-		$('body').toggleClass('menu-open');
-	});
+    // toggle class to control open/close of off-canvas
+    $('.open-menu').click(function() {
+        $('body').toggleClass('menu-open');
+    });
 
-	$(window).resize(function() {
-		if ($(window).width() < 978) {
-			$('body').removeClass('menu-open');
-		}
-	});
+    // Closes the off-canvas menu on window resize
+    $(window).resize(function() {
+        if ($(window).width() < 978) {
+            $('body').removeClass('menu-open');
+        }
+    });
 
-	$('.mobile-menu li a').click(function(){
-		$('body').removeClass('menu-open');
-	});
+    // Auto close off-canvas menu when item is clicked
+    $('.mobile-menu li a').click(function() {
+        $('body').removeClass('menu-open');
+    });
+
+    // Toggle class on body when site is at top position
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= 100) {
+            $('body').addClass('changed');
+        } else {
+            $('body').removeClass('changed');
+        }
+    });
 
 });
--- client/assets/js/slideIn.js
+++ client/assets/js/slideIn.js
@@ -1,16 +1,18 @@
 (function($) {
 
-    $.fn.visible = function(partial) {
+$.fn.visible = function(partial) {
 
-        var $t = $(this),
-            $w = $(window),
-            viewTop = $w.scrollTop(),
-            viewBottom = viewTop + $w.height(),
-            _top = $t.offset().top,
-            _bottom = _top + $t.height(),
-            compareTop = partial === true ? _bottom : _top,
-            compareBottom = partial === true ? _top : _bottom;
-        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    var $t = $(this),
+        $w = $(window),
+        lastScrollTop = 0;
+        viewTop = $w.scrollTop(),
+        viewBottom = viewTop + $w.height(),
+        _top = $t.offset().top,
+        _bottom = _top + $t.height(),
+        compareTop = partial === true ? _bottom : _top,
+        compareBottom = partial === true ? _top : _bottom;
+
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
     };
 
@@ -19,6 +21,32 @@
 var win = $(window);
 var rightModule = $('.project:odd');
 var leftModule = $('.project:even');
+
+var skillLevel1 = $('.skill-lvl-1');
+var skillLevel2 = $('.skill-lvl-2');
+var skillLevel3 = $('.skill-lvl-3');
+
+
+skillLevel1.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+        el.addClass("already-visible");
+    }
+});
+
+skillLevel2.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+        el.addClass("already-visible");
+    }
+});
+
+skillLevel3.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+        el.addClass("already-visible");
+    }
+});
 
 rightModule.each(function(i, el) {
     var el = $(el);
@@ -35,6 +63,27 @@ leftModule.each(function(i, el) {
 });
 
 win.scroll(function(event) {
+
+    skillLevel1.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("skill-lvl-1-active");
+        }
+    });
+
+    skillLevel2.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("skill-lvl-2-active");
+        }
+    });
+
+    skillLevel3.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("skill-lvl-3-active");
+        }
+    });
 
     rightModule.each(function(i, el) {
         var el = $(el);
